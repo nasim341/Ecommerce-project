02e78e3a1b584848db66a2ac966460ae2581c920
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-03-16 07:19:58 -0400

    nav-menu adding

---
+++ Ecommerce/frontend/src/components/nav/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+const Menu = () => {
+    return (
+
+        <ul className="nav d-flex justify-content-between shadow-sm mb-2 sticky-top bg-light">
+            <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
+                    Home
+                </NavLink>
+            </li>
+            <li className='nav-item'>
+                <NavLink className="nav-link" to="/login">
+                    Login
+                </NavLink>
+            </li>
+            <li className='nav-item'>
+                <NavLink className="nav-link" to="/register">
+                    Register
+                </NavLink>
+            </li>
+        </ul>
+
+
+    );
+};
+export default Menu;

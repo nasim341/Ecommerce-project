9290344ccc1976bd790fd9fe1f027b586b017198
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-03-24 15:25:15 -0400

    context-auth.js add

--- Ecommerce/frontend/src/index.js
+++ Ecommerce/frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/auth';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
